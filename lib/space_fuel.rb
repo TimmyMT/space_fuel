424b63a8e4369bf2a9c8063df366902998d55c28
TimmyMT <mathcoredetected@gmail.com>
2023-06-11 13:39:28 +0500

    fixed funcional

--- lib/space_fuel.rb
+++ lib/space_fuel.rb
@@ -12,12 +12,10 @@ module SpaceFuel
       @ship_mass = ship_mass
       @flight_route = flight_route
       @total_fuel = 0
-      @additional_fuel = 0
     end
 
     def call
-      calculate_fuel_for_route
-      @total_fuel += @additional_fuel
+      fuel_by_route
       @total_fuel
     end
 
@@ -25,31 +23,28 @@ module SpaceFuel
 
     FUEL_STATS = { launch: [0.042, 33], land: [0.033, 42] }.freeze
 
-    def calculate_fuel_for_route
+    def fuel_by_route
       @flight_route.each do |step|
         directive, gravity = step
 
-        fuel = calculate_fuel(@ship_mass, gravity, directive)
-        update_fuel_counters(directive, fuel)
+        calculate_fuel(@ship_mass, gravity, directive)
       end
     end
 
     def calculate_fuel(ship_mass, gravity, directive)
+      init_fuel = formula(ship_mass, gravity, directive)
+      last_fuel = 0
+
+      while init_fuel > 40
+        @total_fuel += init_fuel if last_fuel != init_fuel
+        init_fuel = formula(init_fuel, gravity, directive)
+        @total_fuel += init_fuel
+        last_fuel = init_fuel
+      end
+    end
+
+    def formula(ship_mass, gravity, directive)
       (ship_mass * gravity * FUEL_STATS[directive][0] - FUEL_STATS[directive][1]).to_i
-    end
-
-    def update_fuel_counters(directive, fuel)
-      send("update_#{directive}_fuel", fuel)
-    end
-
-    def update_launch_fuel(fuel)
-      @additional_fuel += fuel
-    end
-
-    def update_land_fuel(fuel)
-      @total_fuel += fuel
-      @additional_fuel -= fuel
-      @additional_fuel = 0 if @additional_fuel.negative?
     end
   end
 end
--- spec/space_fuel_spec.rb
+++ spec/space_fuel_spec.rb
@@ -10,10 +10,18 @@ RSpec.describe SpaceFuel::Calculator do
       end
     end
 
+    context 'with land Apollo 11 CSM on the Earth' do
+      let(:ship_mass) { 28801 }
+      let(:flight_route) { [[:land, 9.807]] }
+      let(:expected_fuel) { 13447 }
+
+      include_examples 'calculates the fuel required correctly'
+    end
+
     context 'with Apollo 11 mission' do
       let(:ship_mass) { 28801 }
       let(:flight_route) { [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 9.807]] }
-      let(:expected_fuel) { 13755 }
+      let(:expected_fuel) { 36752 }
 
       include_examples 'calculates the fuel required correctly'
     end
@@ -21,7 +29,7 @@ RSpec.describe SpaceFuel::Calculator do
     context 'with Mission on Mars' do
       let(:ship_mass) { 14606 }
       let(:flight_route) { [[:launch, 9.807], [:land, 3.711], [:launch, 3.711], [:land, 9.807]] }
-      let(:expected_fuel) { 8226 }
+      let(:expected_fuel) { 21029 }
 
       include_examples 'calculates the fuel required correctly'
     end
@@ -29,7 +37,7 @@ RSpec.describe SpaceFuel::Calculator do
     context 'with Passenger ship' do
       let(:ship_mass) { 75432 }
       let(:flight_route) { [[:launch, 9.807], [:land, 1.62], [:launch, 1.62], [:land, 3.711], [:launch, 3.711], [:land, 9.807]] }
-      let(:expected_fuel) { 47858 }
+      let(:expected_fuel) { 121712 }
 
       include_examples 'calculates the fuel required correctly'
     end
